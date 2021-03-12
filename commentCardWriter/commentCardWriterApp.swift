ee41226f5abde2ce401fd77d856d05f1899eaf34
Aasmaan Yadav <yadav.a@etoncollege.org.uk>
2021-03-12 12:17:38 +0000

    Initial Commit

---
+++ commentCardWriter/commentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  commentCardWriterApp.swift
+//  commentCardWriter
+//
+//  Created by Yadav, Aasmaan (SPH) on 12/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct commentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
